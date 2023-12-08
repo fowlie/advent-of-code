a841b6f43e9845dcda153ea1d2a23122c8249a5c
Mats Faugli <mats.faugli@gmail.com>
2023-12-08 16:08:27 +0100

    Solve day3 part1

--- 03/part1.py
+++ 03/part1.py
@@ -1,5 +1,3 @@
-import re  # Regular Expressions
-
 parts = []
 lines = []
 with open("input") as input:
@@ -7,39 +5,31 @@ with open("input") as input:
         lines.append(line.strip())
 
 
-def is_symbol(c):
-    return not c.isdigit() and c != "."
+for i in range(0, len(lines)):  # for each line
+    parsed_number = ""
 
+    for j in range(0, len(lines[i])):  # for each char on line
+        if lines[i][j].isdigit():
+            parsed_number += lines[i][j]
 
-def symbol_in_range(i, start, end):
-    if i < 0 or i >= len(lines):
-        return False
+            if j < len(lines[i]) - 1:  # prevent index out of bounds
+                if lines[i][j + 1].isdigit():  # next also digit?
+                    continue
 
-    # print(f"checking line {i+1} for symbols in range {start}-{end}")
-    line = lines[i]
-    for i in range(start, end):
-        if is_symbol(line[i]):
-            return True
-    return False
+            # define the "width" of the scan field
+            # use min() and max() to avoid indexoutofbounds
+            start = max(j - len(parsed_number), 0)
+            end = min(j + 2, len(lines[i]))
 
+            # scan for symbols in field around number
+            for x in range(i - 1, i + 2):
+                for y in range(start, end):
+                    if x < 0 or x >= len(lines):
+                        continue
+                    if not lines[x][y].isdigit() and lines[x][y] != ".":
+                        parts.append(int(parsed_number))
+                        break
+            parsed_number = ""
 
-for i, line in enumerate(lines):
-    numbers = re.findall(r"\d+", line)
-    for number in numbers:
-        pos = line.find(number)
-
-        start = max(pos - 1, 0)
-        end = min(pos + len(number) + 1, len(lines[i]))
-        if (
-            symbol_in_range(i, start, end)
-            or symbol_in_range(i - 1, start, end)
-            or symbol_in_range(i + 1, start, end)
-        ):
-            parts.append(number)
-            # print(f"{number} on line {i+1} is a part number")
-        # else:
-        # print(f"{number} on line {i+1} is NOT a part number")
-
-
-# sum all numbers in list
+# correct answer is 539590
 print("the answer is", sum([int(i) for i in parts]))
